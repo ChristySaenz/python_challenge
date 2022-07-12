37de2ac97d6443ad2d1a72a3f35d70cb99b1c0f5
ChristySaenz <christina.m.saenz@gmail.com>
2022-07-11 22:28:25 -0500

    Both Sum of profit/Loss and total months working

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,32 +4,33 @@ import csv
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
 PL_Values = []
-Date = []
 PL_Total =[]
 # Date formatted as MonthAbv - Year (last 2 numbers)
 # Open and read csv
+Months = 0
+PL_Total = 0
 with open(budget_csv) as csvfile:
     csvreader =csv.reader(csvfile, delimiter=",")
-    print(csvreader)
+    #print(csvreader)
     # Read the header row first
     csv_header = next(csvreader) 
-    print(f"CSV Header: {csv_header}")
+    print(f"CSV Header: {csv_header}") 
 #Calculate Profit/Losses total  
-    PL_Total = 0
-    for col in csvreader:
-        #print(col[1])
-        PL_Total += int(col[1])
-    print(PL_Total)
-
+    for row in csvreader:
+        PL_Total += int(row[1])
+        Months += 1
+    print (f"Total Months: {Months}")    
+    #for col in csvreader:
+    print(f"Total: {PL_Total}")
+   
         
-        
+    
 
     
-  
 
 #Identify the total # of Months
-#total_months =  str(len(Date))  
-#print("Total Months: " + total_months)
+       
+
               
 #Identify the net_total of Profit/Losses
 
