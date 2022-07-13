2e51a114f21510c3aef5605e4c6c077d944c2011
ChristySaenz <christina.m.saenz@gmail.com>
2022-07-13 16:58:15 -0500

    Cleaned and complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,16 +8,13 @@ PL_Total =[]
 Month_Diff = []
 Month = []
 Fin_Analysis = []
-# Date formatted as MonthAbv - Year (last 2 numbers)
-# Open and read csv
 Months = 0
 PL_Total = 0
+# Open and read csv
 with open(budget_csv) as csvfile:
     csvreader =csv.reader(csvfile, delimiter=",")
-    #print(csvreader)
     # Read the header row first
-    csv_header = next(csvreader) 
-    #print(f"CSV Header: {csv_header}") 
+    csv_header = next(csvreader)  
 #Calculate Profit/Losses total  
 #Used this stackoverflow thread to adjust code to work for find inc/dec instructions
 #https://stackoverflow.com/questions/46965192/python-how-can-i-find-difference-between-two-rows-of-same-column-using-loop-in
@@ -32,8 +29,8 @@ with open(budget_csv) as csvfile:
 
     Dollar_total = "${:,.2f}".format(PL_Total)
 
-    Fin_Analysis.append(f"Financial Analysis")
-    Fin_Analysis.append(f"------------------------------")
+    Fin_Analysis.append("Financial Analysis")
+    Fin_Analysis.append("------------------------------")
     Fin_Analysis.append(f"Total Months: {Months}")    
     Fin_Analysis.append(f"Total: {Dollar_total}")
 # Setting row count as i by defining the length of the column at each iteration
@@ -44,44 +41,34 @@ with open(budget_csv) as csvfile:
 # Average Profit/Losses Changes
 # #Finding Average change by taking the sum of differences divided by number of months minus due to first month assumed as "no change"
         Avg_Change = "${:,.2f}".format(sum(Month_Diff)/(Months-1))
-    #print (f"{i} : {Month_Diff}")
     Fin_Analysis.append(f"Average Change: {Avg_Change}")
    
-
-#https://www.delftstack.com/howto/python/python-max-value-in-list/
-# pos_pl_value = []
-# neg_pl_value = []
-# for x in PL_Value:
-#     if (x >= 0):
-#         pos_pl_value.append(x)
-#     else:
-#         neg_pl_value.append(x)
-#     print(max(pos_pl_value))
-#     print(max(neg_pl_value))
-#Identify the greatest increase (Date and amount)
-#Identify the greatest decrease (Date and amount)
 # Calculated max and min then idetified value for Month (row [0]) + 1 since the difference needs to start one row down from the first row.
 max_diff = None
 min_diff = None
-for value in Month_Diff: 
+for value in Month_Diff:
+#Identify the greatest increase (Date and amount) 
     if(max_diff is None or value > max_diff):
         max_diff = value
         max_diff_dol = "${:,.2f}".format(max_diff)
         Max_diff_month = str(Month[Month_Diff.index(max_diff) + 1])
+#Identify the greatest decrease (Date and amount)
     if(min_diff is None or value < min_diff): 
         min_diff = value
         min_diff_dol = "${:,.2f}".format(min_diff)
         min_diff_month = str(Month[Month_Diff.index(min_diff) + 1])
 Fin_Analysis.append(f"Greatest increase in Profits: {Max_diff_month} ({max_diff_dol})")
 Fin_Analysis.append(f"Greatest decrease in Profits: {min_diff_month} ({min_diff_dol})")
+
 #https://www.w3resource.com/python-exercises/list/python-data-type-list-exercise-168.php
+#Print each element on own line
 for p in Fin_Analysis:
     print(p)
-
 
 #Export results to text file
 Final_Analysis = os.path.join("analysis","Final_Analysis.txt")  
 #https://stackoverflow.com/questions/7138686/how-to-write-a-list-to-a-file-with-newlines-in-python3
+#With each element on own line
 with open(Final_Analysis, mode ="wt") as f:
     for p in Fin_Analysis:
         f.write(p)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,8 +4,6 @@ import csv
 
 elec_csv = os.path.join("Resources", "election_data.csv")
 Voters = []
-County = []
-U_Counties = []
 Canidate = []
 U_Canidates = []
 CCS_Votes = 0
@@ -20,66 +18,52 @@ with open(elec_csv) as csvfile:
 # The total number of votes cast
     for row in csvreader:
         Voters.append(int(row[0]))
-        County.append(row[1])
         Canidate.append(row[2])
-    total_votes = len(Voters)    
+    total_votes = len(Voters)
+#Identifying name of all canidates        
     for c in Canidate:
         if c not in U_Canidates:
             U_Canidates.append(c)  
 election_results.append("Election Results")
 election_results.append("-------------------------")
 election_results.append(f"Total Votes: {total_votes}")
-# A complete list of candidates who received votes
-#print(U_Canidates)
-# The percentage of votes each candidate won
+# A complete list of candidates who received votes 
+# The total number of votes each candidate won
 for v in Canidate:
-    if v == "Charles Casper Stockham":
+    if v == "Charles Casper Stockham":  
         CCS_Votes = CCS_Votes + 1
     if v == "Diana DeGette":
         DDG_Votes =DDG_Votes + 1
     if v == "Raymon Anthony Doane":
         RAD_Votes =RAD_Votes + 1
+# The percentage of votes each candidate won
 per_votes_CCS = "{0:.3f}%".format((CCS_Votes/total_votes) * 100)
 per_votes_DDG = "{0:.3f}%".format((DDG_Votes/total_votes) * 100)
 per_votes_RAD = "{0:.3f}%".format((RAD_Votes/total_votes) * 100)
-# The total number of votes each candidate won
+
 election_results.append("-------------------------")
 election_results.append(f"Charles Casper Stockham: {per_votes_CCS} ({CCS_Votes})")
 election_results.append(f"Diana DeGette: {per_votes_DDG} ({DDG_Votes})")
 election_results.append(f"Raymon Anthony Doane: {per_votes_RAD} ({RAD_Votes})")
 election_results.append("-------------------------")
+
 # The winner of the election based on popular vote.
-
-# per_votes = [CCS_Votes, DDG_Votes, RAD_Votes]
-# Winner = None
-# w = 0
-# for p in per_votes:
-#     if (w == 0 or p > Winner):
-#        w = p
-#https://www.codegrepper.com/code-examples/python/how+to+find+most+repeated+word+in+a+string+in+python
-# from collections import counter
-# name_count = Counter(Canidate)
-# winner_name = Counter.most_common(4)
-# print(winner_name)
-
 #https://www.geeksforgeeks.org/python-find-most-frequent-element-in-a-list/
-# Program to find most frequent
-# element in a list
+    # Program to find most frequent element in a list
  
 def winner_name(List):
     return max(set(List), key = List.count)
 List = Canidate
 winner=(winner_name(List))
-  
-
 
 election_results.append(f"Winner: {winner} ")
 election_results.append("-------------------------")
 
+# Have each element print on separate line
 for p in election_results:
     print(p)
 
-# #Export results to text file
+# #Export results to text file (with each element on separate line)
 fin_elec_results = os.path.join("analysis","Final_Results.txt")  
 # #https://stackoverflow.com/questions/7138686/how-to-write-a-list-to-a-file-with-newlines-in-python3
 with open(fin_elec_results, mode ="wt") as f:
